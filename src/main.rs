7952b4d9100709046f6c72c2c061d06025ccf880
Joseph Cheverton-Wynne <jchevertonwynne@gmail.com>
2021-02-23 12:37:03 +0000

    removed unnecessary channel and minor fixups

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,17 @@ use std::{
 
 use tokio::{
     self,
-    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader, BufWriter},
     net::{
         tcp::{ReadHalf, WriteHalf},
         TcpListener,
     },
+    select,
     sync::{
         mpsc::{self, Receiver, Sender},
         oneshot,
     },
+    time::{Duration, Instant},
 };
 
 enum MessageType {
@@ -56,9 +58,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     resp.send(()).unwrap();
                 }
                 MessageType::Sub(topic, sender, resp) => {
-                    let ent = pubsub.topics.entry(topic).or_default();
-                    let size = ent.len();
-                    ent.insert(size, sender);
+                    let subscribers = pubsub.topics.entry(topic).or_default();
+                    let size = subscribers.len();
+                    subscribers.insert(size, sender);
                     resp.send(size).unwrap();
                 }
                 MessageType::Unsub(topic, ind, resp) => {
@@ -77,47 +79,51 @@ async fn main() -> Result<(), Box<dyn Error>> {
         let pubsub_send = pubsub_send.clone();
 
         tokio::spawn(async move {
-            let (mut conn_read, mut conn_write) = conn.split();
+            let (conn_read, conn_write) = conn.split();
             let (conn_to_ps_send, conn_to_ps_rec): (Sender<String>, Receiver<String>) =
                 mpsc::channel(10);
-            let (closer_send, closer_recv): (
-                tokio::sync::oneshot::Sender<()>,
-                tokio::sync::oneshot::Receiver<()>,
-            ) = tokio::sync::oneshot::channel();
 
-            let w = write_out(&mut conn_write, conn_to_ps_rec, closer_send);
-            let r = read_in(&mut conn_read, conn_to_ps_send, pubsub_send, closer_recv);
+            let w = write_out(conn_write, conn_to_ps_rec);
+            let r = read_in(conn_read, conn_to_ps_send, pubsub_send);
 
             futures::future::join(w, r).await;
         });
     }
 }
 
-async fn write_out(
-    conn_write: &mut WriteHalf<'_>,
-    mut conn_to_ps_rec: Receiver<String>,
-    mut closer: tokio::sync::oneshot::Sender<()>,
-) {
+async fn write_out(conn_write: WriteHalf<'_>, mut conn_to_ps_rec: Receiver<String>) {
+    let mut writer = BufWriter::new(conn_write);
+    let mut limit = Instant::now() + Duration::from_millis(500);
+
     loop {
-        tokio::select! {
+        select! {
             msg = conn_to_ps_rec.recv() => {
                 if let Some(mut msg) = msg {
                     msg.push('\n');
-                    if let Err(err) = conn_write.write(msg.as_bytes()).await {
+                    if let Err(err) = writer.write(msg.as_bytes()).await {
                         eprintln!("write out error - {}", err.to_string());
                     }
+                } else {
+                    if let Err(err) = writer.flush().await {
+                        eprintln!("{}", err.to_string());
+                    }
+                    return;
                 }
             }
-            _ = closer.closed() => return
+            _ = tokio::time::sleep_until(limit) => {
+                if let Err(err) = writer.flush().await {
+                    eprintln!("{}", err.to_string());
+                }
+                limit += Duration::from_millis(500);
+            }
         }
     }
 }
 
 async fn read_in(
-    conn_read: &mut ReadHalf<'_>,
+    conn_read: ReadHalf<'_>,
     conn_to_ps_send: Sender<String>,
     pubsub_send: Sender<MessageType>,
-    mut closer: tokio::sync::oneshot::Receiver<()>,
 ) {
     let mut user_input = String::new();
     let mut reader = BufReader::new(conn_read);
@@ -143,22 +149,22 @@ async fn read_in(
 
         match parts[0].as_str() {
             "p" | "pub" | "publish" if parts.len() == 3 => {
-                let (os, or) = oneshot::channel();
-                let action = MessageType::Pub(parts[1].clone(), parts[2].clone(), os);
+                let (notify_done, done) = oneshot::channel();
+                let action = MessageType::Pub(parts[1].clone(), parts[2].clone(), notify_done);
                 if let Err(err) = pubsub_send.send(action).await {
                     eprintln!("pub send error - {}", err.to_string());
                 }
-                if let Err(err) = or.await {
+                if let Err(err) = done.await {
                     eprintln!("pub oneshot error - {}", err.to_string());
                 }
             }
             "s" | "sub" | "subscribe" if parts.len() == 2 => {
-                let (os, or) = oneshot::channel();
-                let action = MessageType::Sub(parts[1].to_string(), conn_to_ps_send.clone(), os);
+                let (notify_done, done) = oneshot::channel();
+                let action = MessageType::Sub(parts[1].to_string(), conn_to_ps_send.clone(), notify_done);
                 if let Err(err) = pubsub_send.send(action).await {
                     eprintln!("set send error - {}", err.to_string());
                 }
-                let ind = match or.await {
+                let ind = match done.await {
                     Ok(ind) => ind,
                     Err(err) => {
                         eprintln!("set oneshot error - {}", err.to_string());
@@ -168,20 +174,23 @@ async fn read_in(
                 positions.insert(parts[1].clone(), ind);
             }
             "u" | "unsub" | "unsubscribe" if parts.len() == 2 => {
-                let (os, or) = oneshot::channel();
-                let ind = *positions.get(&parts[1]).unwrap();
-                let action = MessageType::Unsub(parts[1].clone(), ind, os);
+                let (notify_done, done) = oneshot::channel();
+                let ind = match positions.get(&parts[1]) {
+                    Some(&ind) => ind,
+                    None => continue
+                };
+                let action = MessageType::Unsub(parts[1].clone(), ind, notify_done);
                 if let Err(err) = pubsub_send.send(action).await {
-                    eprintln!("uns send error - {}", err.to_string())
+                    eprintln!("unsub send error - {}", err.to_string())
                 }
-                if let Err(err) = or.await {
-                    eprintln!("ubs oneshot error - {}", err.to_string());
+                if let Err(err) = done.await {
+                    eprintln!("unsub oneshot error - {}", err.to_string());
                 }
                 positions.remove(&parts[1]);
             }
             _ => {
                 if let Err(err) = conn_to_ps_send.send("bad input command, please pub `p subject message`, sub `s topic` or unsub `u topic`".into()).await {
-                    eprintln!("uns send error - {}", err.to_string())
+                    eprintln!("unsub send error - {}", err.to_string())
                 }
             }
         }
@@ -191,12 +200,10 @@ async fn read_in(
         let (os, or) = oneshot::channel();
         let action = MessageType::Unsub(topic, ind, os);
         if let Err(err) = pubsub_send.send(action).await {
-            eprintln!("uns send error - {}", err.to_string())
+            eprintln!("unsub send error - {}", err.to_string())
         }
         if let Err(err) = or.await {
-            eprintln!("ubs oneshot error - {}", err.to_string());
+            eprintln!("unsub oneshot error - {}", err.to_string());
         }
     }
-
-    closer.close();
 }
